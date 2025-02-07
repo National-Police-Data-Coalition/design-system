c1aad9602d39da87ae0748b2eb72d7ac6831c259
Samantha Sexton <50561643+srsexton94@users.noreply.github.com>
2025-02-06 19:48:04 -0500

    create component library page

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
-import npdcLogo from './assets/npdc.png'
-import './App.css'
+import npdcLogo from "./assets/npdc.png";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <div>
-        <a href="https://www.nationalpolicedata.org" target="_blank">
-          <img src={npdcLogo} className="logo" alt="Vite logo" />
+    <header>
+      <div className="logoText">
+        <div>
+          <a href="https://www.nationalpolicedata.org" target="_blank">
+            <img src={npdcLogo} className="logo" alt="Vite logo" />
+          </a>
+        </div>
+        <div>
+          <h1>National Police Data Index</h1>
+          <h2>Design System</h2>
+        </div>
+      </div>
+      <div className="card">
+        <a href="https://npdindex.readme.io/reference/getting-started-with-your-api">
+          <button>Learn More</button>
         </a>
       </div>
-      <h1>National Police Data Index</h1>
-      <h2>Design System</h2>
-      <div className="card">
-        <a href="https://npdindex.readme.io/reference/getting-started-with-your-api"><button>
-          Learn More
-        </button></a>
-      </div>
+    </header>
+    <section className="componentLibrary">
+
+    </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
