b09158dbe81ff82857ea5676761248b51e663589
srsexton94 <srsexton94@gmail.com>
2025-02-18 21:01:30 -0500

    implement storybook

--- src/component-library/index.ts
+++ src/component-library/index.ts
@@ -1,3 +1,3 @@
-import TextInput from "./text-input/TextInput";
+import TextInput from "./TextInput/TextInput";
 
 export { TextInput };
---
+++ src/stories/TextInput.stories.tsx
@@ -0,0 +1,21 @@
+import { ComponentProps } from "react";
+import { Meta, StoryObj } from "@storybook/react";
+import { TextInput } from "../component-library";
+
+type StoryProps = ComponentProps<typeof TextInput>;
+
+const meta: Meta<StoryProps> = {
+  component: TextInput,
+};
+
+export default meta;
+
+type Story = StoryObj<StoryProps>;
+
+export const Text: Story = {
+  args: {
+    label: "Name",
+    isFull: false,
+  },
+  render: (args) => <TextInput {...args} />,
+};
